5852c966ab156d4b8c35cf7d0d8292869ee026fe
Zhipeng Jia <zhipeng.jia@outlook.com>
2021-05-22 04:14:19 -0500

    Separate jdk8 and jdk11

--- java-dev/build.sh
+++ java-dev/build.sh
@@ -2,9 +2,10 @@
 
 SCRIPT_PATH=$(readlink -f $0)
 BASE_DIR=$(dirname $SCRIPT_PATH)
-TAG=idea-community
 
 ( cd $BASE_DIR &&
-  DOCKER_BUILDKIT=1 docker build -t zjia/java-dev:$TAG . )
+  DOCKER_BUILDKIT=1 docker build -t zjia/java-dev:jdk8-idea  -f Dockerfile.jdk8 . &&
+  DOCKER_BUILDKIT=1 docker build -t zjia/java-dev:jdk11-idea -f Dockerfile.jdk11 . )
 
-docker push zjia/java-dev:$TAG
+docker push zjia/java-dev:jdk8-idea
+docker push zjia/java-dev:jdk11-idea
