4fd9796ff51e93cdd156ae204fb7747c3ad7c34e
Zhipeng Jia <zhipeng.jia@outlook.com>
2022-01-01 19:39:50 -0600

    Export DEVUSER env variable

--- dev-container/setup-ssh-pubkey.sh
+++ dev-container/setup-ssh-pubkey.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+KEYFILE=/home/${DEVUSER}/.ssh/authorized_keys
+
 if [ -n "${SSH_PUBKEY}" ]; then
-    echo "${SSH_PUBKEY}" > ~/.ssh/authorized_keys
+    echo "${SSH_PUBKEY}" > ${KEYFILE}
     exit 0
 fi
 
 if [ -f /etc/container_environment/SSH_PUBKEY ]; then
-    cat /etc/container_environment/SSH_PUBKEY > ~/.ssh/authorized_keys
+    cat /etc/container_environment/SSH_PUBKEY > ${KEYFILE}
     exit 0
 fi
 
 if [ -f /tmp/id_rsa.pub ]; then
-    cat /tmp/id_rsa.pub > ~/.ssh/authorized_keys
+    cat /tmp/id_rsa.pub > ${KEYFILE}
     exit 0
 fi
 
