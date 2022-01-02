bab28d165b5611d5790e4010f3e56813581ed02b
Zhipeng Jia <zhipeng.jia@outlook.com>
2022-01-01 18:47:23 -0600

    Cleanup PATH

--- dev-container/setup-user-home.sh
+++ dev-container/setup-user-home.sh
@@ -4,12 +4,14 @@ mkdir ~/.ssh
 touch ~/.ssh/authorized_keys
 
 echo "export LC_ALL=en_US.UTF-8" >> ~/.profile
-
 echo "export EDITOR=nano" >> ~/.profile
 
 mkdir ~/bin
-echo "export PATH=\$PATH:\$HOME/bin" >> ~/.profile
 
-mkdir -p ~/.cache
-mkdir -p ~/.config
-mkdir -p ~/.local/share
+mkdir ~/.cache
+mkdir ~/.config
+
+mkdir ~/.local
+mkdir ~/.local/bin
+mkdir ~/.local/lib
+mkdir ~/.local/share
--- go-dev/setup-user-home.sh
+++ go-dev/setup-user-home.sh
@@ -4,4 +4,4 @@ mkdir ~/go
 
 echo "export GOROOT=/usr/local/go" >> ~/.profile
 echo "export GOPATH=\$HOME/go" >> ~/.profile
-echo "export PATH=\$PATH:\$GOROOT/bin:\$HOME/bin:\$GOPATH/bin" >> ~/.profile
+echo "export PATH=\$GOROOT/bin:\$GOPATH/bin:\$PATH" >> ~/.profile
