87880c2e16440df0e67c5de45e16ab2b00b63169
Zhipeng Jia <zhipeng.jia@outlook.com>
2022-01-01 18:41:03 -0600

    Go dev on top of base container

--- go-dev/create_container.sh
+++ go-dev/create_container.sh
@@ -2,13 +2,14 @@
 
 GO_VERSION="1.17.5"
 GODEV_ROOT=$HOME/go-dev
+USERNAME=devuser
 
 docker create \
     --name go-dev \
     --hostname excalibur-go-dev \
     -p 20022:22 \
     -e SSH_PUBKEY="$(cat ~/.ssh/id_rsa.pub)" \
-    -v ${GODEV_ROOT}/workspace:/home/ubuntu/workspace \
-    -v ${GODEV_ROOT}/gopath:/home/ubuntu/go \
-    -v ${GODEV_ROOT}/vscode:/home/ubuntu/.vscode-server \
+    -v ${GODEV_ROOT}/workspace:/home/${USERNAME}/workspace \
+    -v ${GODEV_ROOT}/gopath:/home/${USERNAME}/go \
+    -v ${GODEV_ROOT}/vscode:/home/${USERNAME}/.vscode-server \
     zjia/go-dev:go${GO_VERSION}
--- go-dev/setup-user-home.sh
+++ go-dev/setup-user-home.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-mkdir ~/.ssh
-touch ~/.ssh/authorized_keys
-
-echo "export LC_ALL=en_US.UTF-8" >> ~/.profile
-
-echo "export EDITOR=nano" >> ~/.profile
-
-mkdir ~/bin
 mkdir ~/go
 
-echo "export GOROOT=/opt/go" >> ~/.profile
+echo "export GOROOT=/usr/local/go" >> ~/.profile
 echo "export GOPATH=\$HOME/go" >> ~/.profile
 echo "export PATH=\$PATH:\$GOROOT/bin:\$HOME/bin:\$GOPATH/bin" >> ~/.profile
-
-mkdir -p ~/.cache
-mkdir -p ~/.config
-mkdir -p ~/.local/share
