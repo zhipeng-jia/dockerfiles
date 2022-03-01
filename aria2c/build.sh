25521103ac726704e083da6bf396c4d36bc74670
Zhipeng Jia <zhipeng.jia@outlook.com>
2022-03-01 02:23:02 -0600

    Update aria2 to 1.36.0

--- aria2c/build.sh
+++ aria2c/build.sh
@@ -2,9 +2,9 @@
 
 SCRIPT_PATH=$(readlink -f $0)
 BASE_DIR=$(dirname $SCRIPT_PATH)
-TAG=1.35.0
+TAG=1.36.0
 
-( cd $BASE_DIR && 
+( cd $BASE_DIR &&
   docker build -t zjia/aria2c:$TAG . )
 
 docker tag zjia/aria2c:$TAG zjia/aria2c:latest
